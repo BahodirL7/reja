29b8446c77d3e1a74935b63a1f15b93413b97176
BahodirL7 <bakhodirtursunovl7@gmail.com>
2024-08-18 17:58:16 +0900

    feat: Set up MongoDB connection and develop app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+console.log("Web serverni boshlash.");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+// MongoDB call
+const db = require("./server").db();
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2: Session
+
+// 3: View code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing code
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+app.get("/", (req, res) => {
+  res.render("rejalar");
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,48 +1,29 @@
-console.log("Web serverni boshlash.");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
+let db;
+const connectionString =
+  "mongodb+srv://bahodir08:bahodir0804@cluster0.zh1in.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0";
 
-fs.readFile("database/user.json", "utf-8", (err, data) => {
-  if (err) {
-    console.log("ERROR", err);
-  } else {
-    user = JSON.parse(data);
+mongodb.connect(
+  connectionString,
+  {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection to MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `This server is running succesfully on port: ${PORT}, http://localhost:${PORT}.`
+        );
+      });
+    }
   }
-});
-
-// 1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 2: Session
-
-// 3: View code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4: Routing code
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", (req, res) => {
-  res.render("rejalar");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `This server is running succesfully on port: ${PORT}, http://localhost:${PORT}.`
-  );
-});
+);
