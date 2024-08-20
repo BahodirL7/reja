4733b4b1389356a416c26d0896ea5fd4b54a353d
BahodirL7 <bakhodirtursunovl7@gmail.com>
2024-08-20 20:54:41 +0900

    feat: Solve A-task algorithmic challenge in train.js

--- train.js
+++ train.js
@@ -1,34 +1,57 @@
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yaxshi talaba boling", // 0-20
-  "togri boshliq tanlang va koproq xato qiling", // 20-30
-  "ozingizga ishlashingizni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yoq endi", // 60
-];
+// A-TASK:
+/*
+Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi so'zdan qatnashgan sonini return qilishi kerak boladi.
+MASALAN countLetter("e", "engineer") 3ni return qiladi.
+*/
 
-// CALLBACK function
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("insert a number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) callback(null, list[1]);
-  else if (a > 30 && a <= 40) callback(null, list[2]);
-  else if (a > 40 && a <= 50) callback(null, list[3]);
-  else if (a > 50 && a <= 60) callback(null, list[41]);
-  else {
-    setInterval(function () {
-      callback(null, list[5]);
-    }, 1000);
-  }
+function countLetter(e, word) {
+  let count = 0;
+  const char = word.split("");
+  char.map((c) => {
+    if (c === e) {
+      count++;
+    }
+  });
+  return count;
 }
 
-maslahatBering(70, (err, data) => {
-  if (err) console.log("ERROR:", err);
-  else {
-    console.log("javob:", data);
-  }
-});
+const result = countLetter("e", "engineeringeeee");
+console.log("A-Task:", result);
+
+// ======= //
+
+// ASYNC & CALLBACK functions
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba boling", // 0-20
+//   "togri boshliq tanlang va koproq xato qiling", // 20-30
+//   "ozingizga ishlashingizni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yoq endi", // 60
+// ];
+
+// // CALLBACK function
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[41]);
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 1000);
+//   }
+// }
+
+// maslahatBering(70, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   else {
+//     console.log("javob:", data);
+//   }
+// });
 
 // ASYNC function
 // async function maslahatBering(a) {
@@ -49,14 +72,14 @@ maslahatBering(70, (err, data) => {
 
 // console.log("passed here 0");
 
-// // then/catch
-// // maslahatBering(25)
-// //   .then((data) => {
-// //     console.log("Javob:", data);
-// //   })
-// //   .catch((err) => {
-// //     console.log("ERROR:", err);
-// //   });
+// then/catch
+// maslahatBering(25)
+//   .then((data) => {
+//     console.log("Javob:", data);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR:", err);
+//   });
 
 // console.log("passed here 1");
 
